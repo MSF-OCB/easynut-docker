4efdd3d8e5f8a2e2ef1d159e8ccd0c916d354b3e
Doudou <edouard.lavaud@gmail.com>
2018-01-07 01:09:00 +0200

    Added full path to rc.local

--- redis/preparehost.sh
+++ redis/preparehost.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
-sed -i -e '$i \echo never > /sys/kernel/mm/transparent_hugepage/enabled\n' rc.local
+sed -i -e '$i \echo never > /sys/kernel/mm/transparent_hugepage/enabled\n' /etc/rc.local
