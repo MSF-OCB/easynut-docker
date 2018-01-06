52973e00e5ad83c79be19b427c24c598a126fef2
Doudou <edouard.lavaud@gmail.com>
2018-01-06 22:56:45 +0200

    Backups now relying on an image, and 2 different CRON added

--- backups/backup_l.sh
+++ backups/backup_l.sh
@@ -3,8 +3,9 @@
 [ -z "${MYSQL_PASSWORD}" ] && { echo "=> MYSQL_PASSWORD cannot be empty" && exit 1; }
 
 DATE=$(date +%Y%m%d%H%M)
-echo "=> Historic backup started at $DATE"
-FILENAME=/backup/$DATE.backup
+echo "=> Backup started at $DATE"
+
+FILENAME=/backup/backup
 if mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --all-databases --add-drop-database --single-transaction --quick --add-locks --disable-keys --extended-insert > "$FILENAME.sql"
 then
   gzip -9 "$FILENAME.sql"
@@ -14,16 +15,6 @@ then
 else
   rm -rf "$FILENAME.sql"
   rm -rf "$FILENAME.sql.gz"
-fi
-
-if [ -n "$MAX_BACKUPS" ]
-then
-  while [ "$(find /backup -maxdepth 1 -name "*.gz.enc" | wc -l)" -gt "$MAX_BACKUPS" ]
-  do
-    TARGET=$(find /backup -maxdepth 1 -name "*.gz.enc" | sort | head -n 1)
-    echo "Backup $TARGET is deleted"
-    rm -rf "$TARGET"
-  done
 fi
 
 echo "=> Backup done"
--- backups/run.sh
+++ backups/run.sh
@@ -2,21 +2,8 @@
 touch /mysql_backup.log
 tail -F /mysql_backup.log &
 
-if [ -n "${INIT_BACKUP}" ]; then
-  echo "=> Create a backup on the startup"
-  /backup.sh
-elif [ -n "${INIT_RESTORE_LATEST}" ]; then
-  echo "=> Restore latest backup"
-  until nc -z "easynut_mysql" "$DB_PORT"
-  do
-      echo "waiting database container..."
-      sleep 1
-  done
-find /backup -maxdepth 1 -name '*.gz.enc' | tail -1 | xargs /restore.sh
-fi
-
-echo "${CRON_TIME} /backup.sh >> /mysql_backup.log 2>&1" > /crontab.conf
+echo "${CRON_TIME} /backup.sh >> /mysql_backup.log 2>&1
+${CRON_TIME_L} /backup_l.sh >> /mysql_backup.log 2>&1" > /crontab.conf
 crontab /crontab.conf
 echo "=> Running cron task manager"
 exec crond -f
-
